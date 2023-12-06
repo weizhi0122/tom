fdde2337887d7a5f61327aba751a42cec4501c41
weizhi0122 <144878692+weizhi0122@users.noreply.github.com>
2023-12-06 13:33:59 +0800

    Add files via upload

---
+++ r13.R
@@ -0,0 +1,53 @@
+{
+  #code lines
+}
+
+for(.x in ...)
+{
+  #code lines
+}
+
+myfun<-functin(...)
+{
+  
+}
+myfun(...) #function call
+
+
+{
+  x+3
+}
+
+value_x<-c(2, 4, 7, 15)
+seq_along(value_x)
+for(x in seq_along(value_x))
+{
+  value_x[[x]]+3
+}
+
+myfun<-function()
+{
+  x+3
+}
+
+x = 17
+myfun()
+
+
+oneComingStudentMajor <- sample(c("Economics", "Sociology"), 1)
+print(oneComingStudentMajor)
+
+if(oneComingStudentMajor=="Economics")
+{
+  # oneComingStudentMajor == "Economics"
+  print("Please go to room 201.")
+}
+
+if(oneComingStudentMajor=="Sociology")
+{
+  # oneComingStudentMajor == "Sociology"
+  print("Please go to room 301.")
+}
+
+#if(...){}
+
